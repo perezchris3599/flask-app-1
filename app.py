77c1824b5c63da7487fdf20bf87edd471a626235
Kweezi Perez Christopher <kweeziperez712@gmail.com>
2021-05-10 19:25:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,10 @@ def yo(name):
 @app.route('/')
 def Home():
     return render_template('index.html',title="Home")
+
+@app.route('/about')
+def about():
+    return render_template('about.html',title="About")
+
 if __name__== '__main__':
     app.run(debug=True)
